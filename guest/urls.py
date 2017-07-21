02b1a04f3b1466388963a8387ff68895227ec016
cenglingming1 <zenglingming@jd.com>
2017-07-21 16:58:41 +0800

    3.3.3关上窗户

--- guest/urls.py
+++ guest/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from sign import views
 
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
     url(r'^index/$', views.index),
     url(r'^login_action/$', views.login_action),
     url(r'^event_manage/$', views.event_manage),
+    url(r'^accounts/login/$', views.index),
 ]
--- sign/views.py
+++ sign/views.py
@@ -1,3 +1,5 @@
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -10,18 +12,20 @@ def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
-        if username == 'admin' and password == 'admin123':
+        # 使用authenticate()函数认证给出的用户名和密码。它接受两个参数，用户名username和密码password
+        # 并在用户名密码正确的情况下返回一个user对象。如果用户名密码不正确，则authenticate()返回None
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)  # 登录
+            request.session['user'] = username  # 将session信息记录到浏览器
             response = HttpResponseRedirect('/event_manage/')
-            # 添加cookie： key，value，有效期
-            #response.set_cookie('user', username, 3600)
-            request.session['user'] = username
             return response
         else:
             return render(request, 'index.html', {'error': 'username or password error!'})
 
 
 # 发布会管理
+@login_required
 def event_manage(request):
-    # username = request.COOKIES.get('user', '')  # 读取浏览器cookie
     username = request.session.get('user', '')
     return render(request, "event_manage.html", {"user": username})
